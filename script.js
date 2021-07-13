c92483c0173eb820329e7d431241ffa1d89d93db
Prithvi <prithvi.patre03@gmail.com>
2021-07-13 07:26:01 +0530

    finished page. can add eraser.

--- script.js
+++ script.js
@@ -26,10 +26,23 @@ change.addEventListener("click", () => {
     makeRows(size, size);
 });
 
-/*
-let removing = getComputedStyle(container).getPropertyValue("--grid-rows");
-for (c=0; c < removing**2; c++){
-  let cell = document.getElementById((c+1));
-  cell.remove(); 
+
+container.addEventListener("mouseover", (e) => {
+    //e (callback function) gives information on the cell clicked. 
+    let cellID = e.path[0]["id"];
+     console.log(cellID);
+     let cell = document.getElementById(cellID);
+     cell.style.cssText = "background-color: black";
+});
+
+function clearing(){
+    let rows = getComputedStyle(container).getPropertyValue("--grid-rows");
+    for (c=0; c < rows**2; c++){
+    let cell = document.getElementById((c+1));
+    cell.style.cssText = "background-color: white"; 
+    };
+
 };
-*/
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", () => clearing() );
